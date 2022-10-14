89896eaf98af24897b5fe001a3cbb1220774f87d
OneNerik <anatoliygoliuk@gmail.com>
2022-10-14 10:59:50 +0300

    bagx

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+// Mobile identificator 
+
+let isMobile = {
+   Android: function () { return navigator.userAgent.match(/Android/i); },
+   BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+   iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+   Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+   Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+   any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+};
+      let body = document.querySelector('body');
+if (isMobile.any()) {
+   body.classList.add('touch');
+} else {
+   body.classList.add('pc');
+}
+
 // Header slider
 
 $(document).ready(function(){
@@ -12,6 +29,15 @@ $(document).ready(function(){
 
 // Header menu
 
+const menuArrow = document.querySelector('.arrow');
+if (menuArrow) {
+   const subList = document.querySelector('.list-header__sub-list');
+   menuArrow.addEventListener("click", function (e) {
+      menuArrow.classList.toggle('active');
+      subList.classList.toggle('active');
+   })
+}
+
 const HeaderIcon = document.querySelector('.nav-header__icon');
 if (HeaderIcon) {
    const ListHeader = document.querySelector('.list-header');
@@ -24,9 +50,17 @@ if (HeaderIcon) {
 
 // Spoilers
 
+if ($(window).width() < 768) {
+   $('.spoiler').toggleClass('one');
+}
+
+
 $(document).ready(function() {
    $('.spoiler__title').click(function(event) {
+      if ($('.spoiler').hasClass('one')) {
+         $('.spoiler__title').not($(this)).removeClass('active');
+         $('.spoiler__text').not($(this).next()).slideUp(300);
+      }
       $(this).toggleClass('active').next().slideToggle(300);
    })
 })
-
